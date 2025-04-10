a98bb5b69c6bba6a769f403173c6caa75451c7bf
Shiv Bardolia <103692458+Shh77hope@users.noreply.github.com>
2025-04-10 10:18:02 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,39 +1,30 @@
 from load_data import load_dataset, CLASS_MAP
 from preprocessing import preprocess_data
-from visualization import (
-    show_samples_per_class,
-    plot_class_distribution,
-    plot_mean_intensity
-)
+from visualization import show_samples_per_class, plot_class_distribution
+from baselineSVMmodel import train_pca_svm
 
 
 def main():
     print(">> Loading data...")
     X_train, y_train, X_test, y_test = load_dataset()
-
     class_names = list(CLASS_MAP.keys())
 
-    print(">> Displaying raw data samples...")
+    print(">> Visualizing raw samples and class distribution...")
     show_samples_per_class(X_train, y_train, class_names)
-
-    print(">> Raw class distribution:")
     plot_class_distribution(y_train, class_names, title="Raw Training Set Distribution")
     plot_class_distribution(y_test, class_names, title="Raw Test Set Distribution")
 
-    print(">> Preprocessing and augmenting data...")
+    print(">> Preprocessing clahe, normalization) and data augmentation...")
     X_train, y_train, X_test, y_test = preprocess_data(X_train, y_train, X_test, y_test)
 
-    print(">> Displaying preprocessed samples...")
+    print(">> Visualizing preprocessed and augmented data...")
     show_samples_per_class(X_train, y_train, class_names)
-
-    print(">> Preprocessed class distribution:")
     plot_class_distribution(y_train, class_names, title="Preprocessed + Augmented Training Set Distribution")
-    plot_class_distribution(y_test, class_names, title="Preprocessed Test Set Distribution")
 
-    print(">> Visualizing average image intensity per class...")
-    plot_mean_intensity(X_train, y_train, class_names)
+    print(">> Training PCA + SVM baseline model...")
+    svm_model = train_pca_svm(X_train, y_train, X_test, y_test, n_components=100)
 
-    print(">> Done.")
+    print(">> All steps completed successfully")
 
 if __name__ == "__main__":
     main()
